839bafc012259efaf7e01c940dfd78ba4bcc36b4
Jeff Brubaker <jtbrubak1@gmail.com>
2017-09-05 02:06:17 -0400

    Add CSV naming

--- scraper.rb
+++ scraper.rb
@@ -1,27 +1,35 @@
-require 'nokogiri'
-require 'mechanize'
+require 'oga'
+require 'open-uri'
 require 'csv'
-require 'byebug'
 
-agent = Mechanize.new
-page = agent.get('https://www.bestbuy.com/site/searchpage.jsp?cp=1&searchType=search&st=smart%20tv&_dyncharset=UTF-8&id=pcat17071&type=page&sc=Global&nrp=&sp=&qp=&list=n&af=true&iht=y&usc=All%20Categories&ks=960&keys=keys')
-# search_form = page.form(name: 'frmSearch')
-# search_form.st = "smart tv"
-# page = agent.submit(search_form)
-File.open('links.txt', 'w') { |file| file << page.links }
-all_results = []
-num_results = page.search(".list-page").attribute('data-num-found').value.to_i
-search_rank = 0
-while all_results.length < num_results
-  search_results = page.search(".list-item")
-  File.open("file.txt", "w") { |file| file << search_results }
-  search_results = search_results.map do |result|
-    brand = result.get_attribute('data-name').split(" - ")[0]
-    model_num = result.search('.model-number .sku-value').text
-    rating = result.search('.star-rating-value').text
-    num_ratings = result.search('.number-of-reviews').text
+def run(search_term)
+  all_results = []
+  page_num = 1
+  page = open(get_url(page_num, search_term))
+  oga_page = Oga.parse_html(page)
+  num_results = oga_page.css(".results-tabs a")[0].attribute('data-count').value.to_i
+  while all_results.length < num_results
+    oga_page = Oga.parse_html(open(get_url(page_num, search_term))) unless all_results.empty?
+    all_results.concat(scrape_page(oga_page, ((page_num - 1) * 24)))
+    page_num += 1
+  end
+  write(all_results)
+end
+
+def get_url(page_num, search_term)
+  "https://www.bestbuy.com/site/searchpage.jsp?cp=#{page_num}&searchType=search&st=#{search_term}&_dyncharset=UTF-8&id=pcat17071&type=page&sc=Global&nrp=&sp=&qp=&list=n&af=true&iht=y&usc=All%20Categories&ks=960&keys=keys"
+end
+
+def scrape_page(oga_page, search_rank)
+  rows = []
+  search_results = oga_page.css(".list-item")
+  search_results.map do |result|
+    brand = result.attribute('data-name').value.split(" - ")[0]
+    model_num = result.css('.model-number .sku-value').text
+    rating = result.css('.star-rating-value').text
+    num_ratings = result.css('.number-of-reviews').text
     search_rank += 1
-    all_results.push({
+    rows.push({
         search_rank: search_rank,
         brand: brand,
         model_num: model_num,
@@ -29,11 +37,17 @@ while all_results.length < num_results
         num_ratings: num_ratings
     })
   end
-  next_link = page.link_with(search: '.pager-next a')
-  page = next_link.click
+  rows
 end
-CSV.open("file.csv", "w") do |csv|
-  all_results.each do |result|
-    csv << [result[:search_rank], result[:brand], result[:model_num], result[:rating], result[:num_ratings]]
+
+def write(results)
+  CSV.open("file1.csv", "w") do |csv|
+    results.each do |result|
+      csv << [result[:search_rank], result[:brand], result[:model_num], result[:rating], result[:num_ratings]]
+    end
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  run(ARGV.join("%20"))
 end
--- scraper1.rb
+++ scraper1.rb
@@ -1,24 +1,36 @@
 require 'oga'
 require 'open-uri'
 require 'csv'
-require 'byebug'
+require 'date'
 
-all_results = []
-page_num = 1
-search_rank = 0
-page = open("https://www.bestbuy.com/site/searchpage.jsp?cp=#{page_num}&searchType=search&st=curved%20smart%20tv&_dyncharset=UTF-8&id=pcat17071&type=page&sc=Global&nrp=&sp=&qp=&list=n&af=true&iht=y&usc=All%20Categories&ks=960&keys=keys")
-oga_page = Oga.parse_html(page)
-num_results = oga_page.css(".results-tabs a")[0].attribute('data-count').value.to_i
-while all_results.length < num_results
-  p num_results
+def run(search_term)
+  all_results = []
+  page_num = 1
+  page = open(get_url(page_num, search_term))
+  oga_page = Oga.parse_html(page)
+  num_results = oga_page.css(".results-tabs a")[0].attribute('data-count').value.to_i
+  while all_results.length < num_results
+    oga_page = Oga.parse_html(open(get_url(page_num, search_term))) unless all_results.empty?
+    all_results.concat(scrape_page(oga_page, ((page_num - 1) * 24)))
+    page_num += 1
+  end
+  write(all_results)
+end
+
+def get_url(page_num, search_term)
+  "https://www.bestbuy.com/site/searchpage.jsp?cp=#{page_num}&searchType=search&st=#{search_term}&_dyncharset=UTF-8&id=pcat17071&type=page&sc=Global&nrp=&sp=&qp=&list=n&af=true&iht=y&usc=All%20Categories&ks=960&keys=keys"
+end
+
+def scrape_page(oga_page, search_rank)
+  rows = []
   search_results = oga_page.css(".list-item")
-  search_results.each do |result|
+  search_results.map do |result|
     brand = result.attribute('data-name').value.split(" - ")[0]
     model_num = result.css('.model-number .sku-value').text
     rating = result.css('.star-rating-value').text
     num_ratings = result.css('.number-of-reviews').text
     search_rank += 1
-    all_results.push({
+    rows.push({
         search_rank: search_rank,
         brand: brand,
         model_num: model_num,
@@ -26,10 +38,19 @@ while all_results.length < num_results
         num_ratings: num_ratings
     })
   end
-  page_num += 1
+  rows
 end
-CSV.open("file1.csv", "w") do |csv|
-  all_results.each do |result|
-    csv << [result[:search_rank], result[:brand], result[:model_num], result[:rating], result[:num_ratings]]
+
+def write(results)
+  file = "#{ARGV.join(" ")} #{DateTime.now.strftime('%m-%d-%Y')}.csv"
+  CSV.open(file, "w") do |csv|
+    csv << ["Search Rank", "Brand", "Model", "Rating", "Number of Ratings"]
+    results.each do |result|
+      csv << [result[:search_rank], result[:brand], result[:model_num], result[:rating], result[:num_ratings]]
+    end
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  run(ARGV.join("%20"))
 end
